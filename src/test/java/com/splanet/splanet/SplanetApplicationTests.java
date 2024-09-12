e278a920a0b6703cd424f6b6cfb3ea42ca4e74ee
kanguk <lm0828@naver.com>
2024-09-12 20:37:53 +0900

    chore: 프로젝트 초기세팅
    Spring Boot 개발을 위한 기본 개발세팅을 진행한다.

---
+++ src/test/java/com/splanet/splanet/SplanetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splanet.splanet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplanetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
