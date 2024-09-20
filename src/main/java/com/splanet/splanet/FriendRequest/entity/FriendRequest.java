78fb0bd821e5a0c9e91982ed36fc84fd0e089725
이세진 <ez23re@gmail.com>
2024-09-20 22:34:30 +0900

    feat: friendRequest 도메인 생성

---
+++ src/main/java/com/splanet/splanet/FriendRequest/entity/FriendRequest.java
@@ -0,0 +1,31 @@
+package com.splanet.splanet.FriendRequest.entity;
+
+import com.splanet.splanet.core.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+@SuperBuilder
+@Getter
+@Setter(value = AccessLevel.PROTECTED)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class FriendRequest extends BaseEntity {
+
+    @Column(name = "requester_id", nullable = false)
+    private Long requesterId;
+
+    @Column(name = "receiver_id", nullable = false)
+    private Long receiverId;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private Status status;
+
+    public enum Status {
+        PENDING,
+        ACCEPTED,
+        REJECTED
+    }
+}
