77efe739abdb357ea4f5bfe822b0253fe81e456c
KimSongMok <mok0618@naver.com>
2024-09-20 22:01:21 +0900

    feat) modify entity, repository

--- src/main/java/com/splanet/splanet/team/entity/Team.java
+++ src/main/java/com/splanet/splanet/team/entity/Team.java
@@ -1,13 +1,15 @@
-package com.splanet.splanet.team;
+package com.splanet.splanet.team.entity;
 
 import com.splanet.splanet.core.BaseEntity;
-import com.splanet.splanet.user.User;
+import com.splanet.splanet.user.entity.User;
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 
 @Getter
+@Entity
 public class Team extends BaseEntity {
   @ManyToOne
   @JoinColumn(name = "user_id", nullable = false)
