9e18e5f3a8daa25684ad24dce7c0a6e5655c934f
KimSongMok <mok0618@naver.com>
2024-09-20 22:01:21 +0900

    feat) modify entity, repository

--- src/main/java/com/splanet/splanet/user/repository/UserRepository.java
+++ src/main/java/com/splanet/splanet/user/repository/UserRepository.java
@@ -1,2 +1,7 @@
-package com.splanet.splanet.user.repository;public class UserRepository {
+package com.splanet.splanet.user.repository;
+
+import com.splanet.splanet.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
 }
