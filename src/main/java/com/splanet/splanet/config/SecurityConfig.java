af23422269ec67f586170cb5b594890a25bdf1a7
kanguk <lm0828@naver.com>
2024-09-26 22:54:34 +0900

    feat: 인증 과정 변경
    Spring Security를 이용하여 토큰을 검증받고, api를 작동시키도록 인증과정을 변경한다.

--- src/main/java/com/splanet/splanet/config/SecurityConfig.java
+++ src/main/java/com/splanet/splanet/config/SecurityConfig.java
@@ -27,7 +27,6 @@ public class SecurityConfig {
     http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
-                    .requestMatchers("/api/admin").authenticated()
                     .anyRequest().permitAll()
             )
             .oauth2Login(oauth2 -> oauth2
