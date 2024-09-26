375c2730ce530355a32f19fa405322244045d042
kanguk <lm0828@naver.com>
2024-09-26 22:54:03 +0900

    feat: user 조회, 삭제, 수정 api 구현
    user 조회,  삭제, 수정 api를 구현한다.

--- src/main/java/com/splanet/splanet/core/BaseEntity.java
+++ src/main/java/com/splanet/splanet/core/BaseEntity.java
@@ -14,7 +14,7 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-@SuperBuilder
+@SuperBuilder(toBuilder = true)
 @Getter
 @Setter(value = AccessLevel.PROTECTED)
 @MappedSuperclass
