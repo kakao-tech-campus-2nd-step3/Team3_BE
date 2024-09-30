5fae97db7770c4ef662c8fc3498384a94d942f6c
KimSongMok <mok0618@naver.com>
2024-09-30 18:21:20 +0900

    Team CRUD 구현 수정
    구현 방법을 수정했고, 팀 관리자 권한 부여 API를 넣었으며, 팀에서 유저를 추가, 제거, 조회할 수 있게 했습니다.

--- src/main/java/com/splanet/splanet/core/exception/ErrorCode.java
+++ src/main/java/com/splanet/splanet/core/exception/ErrorCode.java
@@ -7,9 +7,13 @@ public enum ErrorCode {
     ACCESS_DENIED("권한이 없습니다.", HttpStatus.FORBIDDEN),
     UNAUTHORIZED("인증되지 않은 사용자입니다.", HttpStatus.UNAUTHORIZED),
     ALREADY_CANCELED("이미 취소된 구독입니다.", HttpStatus.BAD_REQUEST),
-    SUBSCRIPSTION_NOT_FOUND("활성화된 구독을 찾을 수 없습니다.", HttpStatus.NOT_FOUND);
+    SUBSCRIPSTION_NOT_FOUND("활성화된 구독을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    TEAM_NOT_FOUND("팀을 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    USER_NOT_FOUND("유저를 찾을 수 없습니다.", HttpStatus.NOT_FOUND),
+    TEAM_MEMBER_NOT_FOUND("해당 유저는 팀에 속해 있지 않습니다.", HttpStatus.NOT_FOUND);
 
-    private final String message;
+
+  private final String message;
     private final HttpStatus status;
 
     ErrorCode(String message, HttpStatus status) {
--- src/main/java/com/splanet/splanet/team/entity/Team.java
+++ src/main/java/com/splanet/splanet/team/entity/Team.java
@@ -1,7 +1,6 @@
 package com.splanet.splanet.team.entity;
 
 import com.splanet.splanet.core.BaseEntity;
-import com.splanet.splanet.user.entity.User;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
@@ -16,19 +15,10 @@ import java.util.List;
 @SuperBuilder
 public class Team extends BaseEntity {
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  @JoinColumn(name = "user_id", nullable = false)
-  private User user;
-
   @Size(max = 100)
   @Column(name = "team_name", nullable = false, length = 100)
   private String teamName;
 
-  @ManyToMany
-  @JoinTable(
-          name = "team_user",
-          joinColumns = @JoinColumn(name = "team_id"),
-          inverseJoinColumns = @JoinColumn(name = "user_id")
-  )
-  private List<User> users;
+  @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
+  private List<TeamUserRelation> teamUserRelations;
 }
