e4fce8228388a552ebc0d1e7049fac1662ab0188
이세진 <ez23re@gmail.com>
2024-10-01 11:35:13 +0900

    refactor: 에러핸들링 사용

--- src/main/java/com/splanet/splanet/payment/service/PaymentService.java
+++ src/main/java/com/splanet/splanet/payment/service/PaymentService.java
@@ -1,6 +1,9 @@
 package com.splanet.splanet.payment.service;
 
-import com.splanet.splanet.payment.dto.PaymentDto;
+import com.splanet.splanet.core.exception.BusinessException;
+import com.splanet.splanet.core.exception.ErrorCode;
+import com.splanet.splanet.payment.dto.PaymentRequest;
+import com.splanet.splanet.payment.dto.PaymentResponse;
 import com.splanet.splanet.payment.entity.Payment;
 import com.splanet.splanet.payment.repository.PaymentRepository;
 import com.splanet.splanet.subscription.entity.Subscription;
@@ -20,12 +23,10 @@ public class PaymentService {
 
     // 결제 생성
     @Transactional
-    public PaymentDto.Response createPayment(Long userId, PaymentDto.CreateRequest request) {
-        // 구독 정보 조회
+    public PaymentResponse createPayment(Long userId, PaymentRequest request) {
         Subscription subscription = subscriptionRepository.findById(request.getSubscriptionId())
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 구독입니다."));
+                .orElseThrow(() -> new BusinessException(ErrorCode.SUBSCRIPSTION_NOT_FOUND));
 
-        // 결제 객체 생성
         Payment payment = Payment.builder()
                 .subscription(subscription)
                 .price(request.getPrice())
@@ -33,26 +34,25 @@ public class PaymentService {
                 .status(Payment.Status.COMPLETED)
                 .build();
 
-        // 결제 저장
         Payment savedPayment = paymentRepository.save(payment);
 
-        // DTO로 변환하여 반환
-        return PaymentDto.Response.fromPayment(savedPayment);
+        return PaymentResponse.fromPayment(savedPayment);
     }
 
     // 결제 삭제
     @Transactional
     public void deletePayment(Long paymentId) {
         Payment payment = paymentRepository.findById(paymentId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 결제입니다."));
+                .orElseThrow(() -> new BusinessException(ErrorCode.PAYMENT_NOT_FOUND));
+
         paymentRepository.delete(payment);
     }
 
     // 결제 상태 조회
-    public PaymentDto.Response getPaymentStatus(Long paymentId) {
+    public PaymentResponse getPaymentStatus(Long paymentId) {
         Payment payment = paymentRepository.findById(paymentId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 결제입니다."));
+                .orElseThrow(() -> new BusinessException(ErrorCode.PAYMENT_NOT_FOUND));
 
-        return PaymentDto.Response.fromPayment(payment);
+        return PaymentResponse.fromPayment(payment);
     }
 }
