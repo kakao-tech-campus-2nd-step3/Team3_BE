14bc0321dfbb944c6e6ab3d4ebc9801e4e969bb2
KimSongMok <mok0618@naver.com>
2024-10-08 12:59:20 +0900

    refactor: Team API 수정
    팀 초대 시, 팀 초대 id 탈취 문제 해결, 로직 수정, 팀 초대 조회 문제 수정

---
+++ src/main/java/com/splanet/splanet/team/dto/TeamInvitationDto.java
@@ -0,0 +1,14 @@
+package com.splanet.splanet.team.dto;
+
+import com.splanet.splanet.team.entity.InvitationStatus;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class TeamInvitationDto {
+  private Long invitationId;
+  private Long teamId;
+  private String teamName;
+  private InvitationStatus status;
+}
--- src/main/java/com/splanet/splanet/team/repository/TeamInvitationRepository.java
+++ src/main/java/com/splanet/splanet/team/repository/TeamInvitationRepository.java
@@ -1,12 +1,14 @@
 package com.splanet.splanet.team.repository;
 
+import com.splanet.splanet.team.entity.InvitationStatus;
 import com.splanet.splanet.team.entity.Team;
 import com.splanet.splanet.team.entity.TeamInvitation;
 import com.splanet.splanet.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface TeamInvitationRepository extends JpaRepository<TeamInvitation, Long> {
-  Optional<TeamInvitation> findByTeamAndUser(Team team, User user);
+  List<TeamInvitation> findAllByUserAndStatus(User user, InvitationStatus status);
 }
