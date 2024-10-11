d82f8625c8229e9fd5aa8c44beed8cb48ce018e5
이세진 <ez23re@gmail.com>
2024-10-11 23:15:39 +0900

    feat: 친구api 구현

--- src/main/java/com/splanet/splanet/friend/controller/FriendApi.java
+++ src/main/java/com/splanet/splanet/friend/controller/FriendApi.java
@@ -1,7 +1,7 @@
 package com.splanet.splanet.friend.controller;
 
 import com.splanet.splanet.friend.dto.FriendResponse;
-import com.splanet.splanet.subscription.dto.SubscriptionResponse;
+import com.splanet.splanet.plan.dto.PlanResponseDto;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -28,15 +28,15 @@ public interface FriendApi {
     ResponseEntity<List<FriendResponse>> getFriends(
             @Parameter(description = "JWT 인증으로 전달된 사용자 ID", required = true) @AuthenticationPrincipal Long userId);
 
-    @GetMapping("/{friend_id}/plans")
-    @Operation(summary = "친구 플랜 조회", description = "친구의 플랜 목록을 조회합니다.")
+    @GetMapping("/{friendId}/plans")
+    @Operation(summary = "친구 플랜 조회", description = "친구의 공개된 플랜 목록을 조회합니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "친구의 공개된 계획이 성공적으로 조회되었습니다."),
             @ApiResponse(responseCode = "400", description = "잘못된 요청입니다 (유효하지 않은 친구 ID)."),
             @ApiResponse(responseCode = "401", description = "인증되지 않은 사용자입니다."),
             @ApiResponse(responseCode = "404", description = "친구를 찾을 수 없습니다.")
     })
-    ResponseEntity<String> getFriendPlan(
+    ResponseEntity<List<PlanResponseDto>> getFriendPlan(
             @Parameter(description = "JWT 인증으로 전달된 친구 ID", required = true) @PathVariable("friend_id") Long friendId,
             @Parameter(description = "JWT 인증으로 전달된 사용자 ID", required = true) @AuthenticationPrincipal Long userId);
 }
