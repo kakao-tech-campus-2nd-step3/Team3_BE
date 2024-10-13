f5772800c3b6f6dfdf825caef05af141d6775e2b
이세진 <ez23re@gmail.com>
2024-10-14 01:20:41 +0900

    refactor: 누락된 트랙재션 어노테이션 추가

--- src/main/java/com/splanet/splanet/friend/service/FriendService.java
+++ src/main/java/com/splanet/splanet/friend/service/FriendService.java
@@ -12,6 +12,7 @@ import com.splanet.splanet.plan.entity.Plan;
 import com.splanet.splanet.plan.repository.PlanRepository;
 import com.splanet.splanet.user.entity.User;
 import com.splanet.splanet.user.repository.UserRepository;
+import jakarta.transaction.Transactional;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -83,7 +84,8 @@ public class FriendService {
         return ResponseEntity.ok(planResponseDtos);
     }
 
-    // 친구 삭제(취소)하기 -> 500 떠서 고치고 다시 푸시할게요!
+    // 친구 삭제(취소)하기
+    @Transactional
     public ResponseEntity<Map<String, String>> unfriend(Long friendId, Long userId) {
         if (!friendRepository.existsByUserIdAndFriendId(userId, friendId)) {
             throw new BusinessException(ErrorCode.FRIEND_NOT_FOUND);
