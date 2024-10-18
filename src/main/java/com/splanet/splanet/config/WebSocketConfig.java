b0d810e6fc0230c8b333c5cda16900353e9202c9
kanguk <lm0828@naver.com>
2024-10-19 02:44:20 +0900

    feat: 실시간 스트리밍 API를 활용하여 STT 구현
    기존 CLOVA Speech Recognition 에서 CLOVA Speech 실시간 스트리밍 API를 활용하여 실시간 처리를 가능하도록 수정한다.

--- src/main/java/com/splanet/splanet/config/WebSocketConfig.java
+++ src/main/java/com/splanet/splanet/config/WebSocketConfig.java
@@ -1,12 +1,10 @@
 package com.splanet.splanet.config;
 
 import com.splanet.splanet.core.handler.SpeechWebSocketHandler;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.standard.ServletServerContainerFactoryBean;
 
 @Configuration
 @EnableWebSocket
@@ -22,13 +20,5 @@ public class WebSocketConfig implements WebSocketConfigurer {
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(speechWebSocketHandler, "/ws/stt")
                 .setAllowedOrigins("*");
-    }
-
-    @Bean
-    public ServletServerContainerFactoryBean configureWebSocketContainer() {
-        ServletServerContainerFactoryBean factory = new ServletServerContainerFactoryBean();
-        factory.setMaxBinaryMessageBufferSize(256 * 1024); //바이너리 버퍼 크기 지정 16KB
-        factory.setMaxTextMessageBufferSize(256 * 1024);  //텍스트 버퍼 크기 지정 16KB
-        return factory;
     }
 }
