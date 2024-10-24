7b28ec4428fb9835734c6c930ac9d15be224896a
이세진 <ez23re@gmail.com>
2024-10-25 01:14:17 +0900

    feat: 스케줄 응답 프롬프트 작성

--- src/main/java/com/splanet/splanet/gpt/ChatResponse.java
+++
@@ -1,9 +0,0 @@
-package com.splanet.splanet.gpt;
-
-import lombok.Getter;
-import java.util.List;
-
-@Getter
-public class ChatResponse {
-    private List<Choice> choices; // Choice 객체 리스트
-}
--- src/main/java/com/splanet/splanet/gpt/Message.java
+++ src/main/java/com/splanet/splanet/gpt/Message.java
@@ -1,12 +1,11 @@
 package com.splanet.splanet.gpt;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-public class Message {
+@AllArgsConstructor
+class Message {
+    private String role; // 메시지의 역할 (user, assistant 등)
     private String content; // 메시지 내용
-
-    public Message(String content) {
-        this.content = content;
-    }
 }
