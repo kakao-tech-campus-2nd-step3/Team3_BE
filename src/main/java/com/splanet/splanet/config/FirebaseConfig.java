b5176233261d391b9d6e5a9ef2a67a781b705dd9
kanguk <lm0828@naver.com>
2024-10-27 15:36:19 +0900

    feat: FCM 초기세팅
    웹 푸시 기능을 구현하기 위해 FCM 초기세팅을 진행한다.

---
+++ src/main/java/com/splanet/splanet/config/FirebaseConfig.java
@@ -0,0 +1,31 @@
+package com.splanet.splanet.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+@Configuration
+public class FirebaseConfig {
+
+    @Bean
+    public FirebaseMessaging firebaseMessaging() throws IOException {
+        GoogleCredentials googleCredentials = GoogleCredentials
+                .fromStream(new ClassPathResource("splanet-cef14-firebase-adminsdk-qe1dd-fcbc5d4a67.json").getInputStream());
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(googleCredentials)
+                .build();
+
+        if (FirebaseApp.getApps().isEmpty()) {
+            FirebaseApp.initializeApp(options);
+        }
+
+        return FirebaseMessaging.getInstance();
+    }
+}
---
+++ src/main/java/com/splanet/splanet/core/fcm/FCMInitializer.java
@@ -0,0 +1,33 @@
+package com.splanet.splanet.core.fcm;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+@Slf4j
+public class FCMInitializer {
+
+    private static final String FIREBASE_CONFIG_PATH = "splanet-cef14-firebase-adminsdk-qe1dd-fcbc5d4a67.json";
+
+    @PostConstruct
+    public void initialize() {
+        try {
+            GoogleCredentials googleCredentials = GoogleCredentials
+                    .fromStream(new ClassPathResource(FIREBASE_CONFIG_PATH).getInputStream());
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(googleCredentials)
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            log.info("FCM initialization error occurred.");
+            log.error("FCM error message : " + e.getMessage());
+        }
+    }
+}
