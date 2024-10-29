0ad6073efdee744d57fe1fa7957e3385062ac117
kanguk <lm0828@naver.com>
2024-10-30 02:54:17 +0900

    bug: CORS 수정
    이전 커밋을 잘못 찍어서 다시 커밋

--- src/main/java/com/splanet/splanet/config/WebConfig.java
+++ src/main/java/com/splanet/splanet/config/WebConfig.java
@@ -10,8 +10,8 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) { //인터페이스 WebMvcConfigurer 상속
         registry.addMapping("/**") //모든 경로를 허용해줄것이므로
-                .allowedOrigins("*") //리소스 공유 허락할 origin 지정
-                .allowedMethods("*") //모든 메소드를 허용
+                .allowedOrigins("http://localhost:5173", "https://splanet.co.kr") //리소스 공유 허락할 origin 지정
+                .allowedMethods("GET", "POST", "PUT", "DELETE") //모든 메소드를 허용
                 .allowCredentials(true);
     }
 }
