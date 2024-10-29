bc8f713d69095fccfcc0033da2f4c7e3ecb5400b
이세진 <ez23re@gmail.com>
2024-10-29 16:44:48 +0900

    feat: SpringAi 연결

--- src/main/java/com/splanet/splanet/config/OpenAiConfig.java
+++ src/main/java/com/splanet/splanet/config/OpenAiConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class OpenAiConfig {
 
-    @Value("${openai.gpt.api-key}")
+    @Value("${spring.ai.openai.api-key}")
     private String apiKey;
 
     @Bean
