2488c0acba6c065f5f75c22025c001c7f275ab2a
kanguk <lm0828@naver.com>
2024-10-31 19:59:57 +0900

    refactor: 모델을 configure에서 받도록 변경

--- src/main/java/com/splanet/splanet/gpt/GptService.java
+++ src/main/java/com/splanet/splanet/gpt/GptService.java
@@ -1,5 +1,6 @@
 package com.splanet.splanet.gpt;
 
+import com.splanet.splanet.core.properties.GptProperties;
 import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -9,14 +10,14 @@ import java.util.List;
 public class GptService {
 
     private final OpenAiApi openAiApi;
+    private final GptProperties gptProperties;
 
-    // GPT 모델과 기본 파라미터 설정
-    private static final String GPT_MODEL = OpenAiApi.ChatModel.GPT_4_O_MINI.getValue();
-    private static final double RESPONSE_TEMPERATURE = 0.8;
-
-    public GptService(OpenAiApi openAiApi) {
+    public GptService(OpenAiApi openAiApi, GptProperties gptProperties) {
         this.openAiApi = openAiApi;
+        this.gptProperties = gptProperties;
     }
+
+    private static final double RESPONSE_TEMPERATURE = 0.8;
 
     public String generateResponse(String inputMessage) {
         // 사용자의 입력을 기반으로 메시지 객체 생성
@@ -26,7 +27,7 @@ public class GptService {
         // 사용자 메시지를 포함하여 ChatCompletionRequest 객체 생성
         OpenAiApi.ChatCompletionRequest chatRequest = new OpenAiApi.ChatCompletionRequest(
                 List.of(userMessage),
-                GPT_MODEL,
+                gptProperties.getGptModel(),
                 RESPONSE_TEMPERATURE
         );
 
