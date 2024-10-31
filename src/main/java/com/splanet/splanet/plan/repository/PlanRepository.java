1e671d4e1473a6a126db4d1cee54ac2d849b1d12
kanguk <lm0828@naver.com>
2024-10-31 21:05:13 +0900

    feat: 사용자의 입력만으로도 프롬프팅문이 알아서 작성되도록 구현
    사용자 입력 뒤에 프롬프팅문을 자동으로 구성하도록 한다. (회원과 비회원을 구분)

--- src/main/java/com/splanet/splanet/plan/repository/PlanRepository.java
+++ src/main/java/com/splanet/splanet/plan/repository/PlanRepository.java
@@ -4,10 +4,12 @@ import com.splanet.splanet.plan.entity.Plan;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
 public interface PlanRepository extends JpaRepository<Plan, Long> {
     List<Plan> findAllByUserId(Long userId);
     List<Plan> findAllByUserIdAndAccessibility(Long userId, Boolean accessibility);
+    List<Plan> findAllByUserIdAndStartDateAfter(Long userId, LocalDateTime currentDateTime);
 }
