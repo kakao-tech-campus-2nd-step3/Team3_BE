4e282f65374f0ed54f3dd3080bcf8f669bb44532
kanguk <lm0828@naver.com>
2024-11-02 21:13:38 +0900

    feat: 최종 무중단배포 CD 스크립트 완성
    완료

--- src/main/java/com/splanet/splanet/config/SwaggerConfig.java
+++ src/main/java/com/splanet/splanet/config/SwaggerConfig.java
@@ -47,7 +47,7 @@ public class SwaggerConfig {
     private Info apiInfo() {
         return new Info()
                 .title("Splanet")
-                .description("Splanet의 API 문서, Version : Green")
+                .description("Splanet의 API 문서")
                 .version("1.0.0");
     }
 }
