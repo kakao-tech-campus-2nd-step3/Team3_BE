a2c62fe1481b910de9f80977679eab998459c30b
이세진 <ez23re@gmail.com>
2024-11-06 00:30:45 +0900

    refactor: 인터페이스 분리 안되어있는 컨트롤러 수정

--- src/main/java/com/splanet/splanet/gpt/controller/GptPlanSaveController.java
+++ src/main/java/com/splanet/splanet/gpt/controller/GptPlanSaveController.java
@@ -1,20 +1,17 @@
 package com.splanet.splanet.gpt.controller;
 
 import com.splanet.splanet.gpt.service.GptPlanSaveService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/gpt/plan")
-@Tag(name = "GPT-Plan save", description = "GPT로 생성된 플랜을 저장한다.")
 @RequiredArgsConstructor
-public class GptPlanSaveController {
+public class GptPlanSaveController implements GptPlanSaveApi{
 
     private final GptPlanSaveService gptPlanSaveService;
 
-    @PostMapping("/save")
+    @Override
     public ResponseEntity<Void> saveGptResponsePlans(@RequestBody String planGroupResponse) {
         gptPlanSaveService.saveGptResponsePlans(planGroupResponse); // JSON 파싱 및 저장 수행
         return ResponseEntity.ok().build();
