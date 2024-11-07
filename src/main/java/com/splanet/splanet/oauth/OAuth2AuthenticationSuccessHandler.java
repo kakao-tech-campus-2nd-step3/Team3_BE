1a3761742503ae4df7eba85dbd3a8dab1acc2991
KimSongMok <mok0618@naver.com>
2024-11-08 06:41:41 +0900

    bug: 버그로 인한 MAU측정로직 철회

--- src/main/java/com/splanet/splanet/oauth/OAuth2AuthenticationSuccessHandler.java
+++ src/main/java/com/splanet/splanet/oauth/OAuth2AuthenticationSuccessHandler.java
@@ -8,16 +8,13 @@ import com.splanet.splanet.user.repository.UserRepository;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.io.FileWriter;
 import java.io.IOException;
-import java.time.Instant;
 import java.util.Map;
 import java.util.UUID;
 
@@ -25,24 +22,24 @@ import java.util.UUID;
 public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccessHandler {
 
   private final JwtTokenProvider jwtTokenProvider;
+
   private final UserRepository userRepository;
   private final TokenService tokenService;
   private final OAuth2Properties oAuth2Properties;
 
-  @Value("${LOG_PATH}")
-  private String logPath;
-
-  public OAuth2AuthenticationSuccessHandler(JwtTokenProvider jwtTokenProvider, UserRepository userRepository,
-                                            TokenService tokenService, OAuth2Properties oAuth2Properties) {
+  public OAuth2AuthenticationSuccessHandler(JwtTokenProvider jwtTokenProvider, UserRepository userRepository, TokenService tokenService, OAuth2Properties oAuth2Properties) {
     this.jwtTokenProvider = jwtTokenProvider;
     this.userRepository = userRepository;
     this.tokenService = tokenService;
     this.oAuth2Properties = oAuth2Properties;
   }
 
+
+
   @Override
   public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
     OAuth2User oAuth2User = (OAuth2User) authentication.getPrincipal();
+
     Map<String, Object> attributes = oAuth2User.getAttributes();
     Map<String, Object> properties = (Map<String, Object>) attributes.get("properties");
 
@@ -69,8 +66,6 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
 
     tokenService.storeRefreshToken(refreshToken, user.getId(), deviceId);
 
-    // 로그 기록 추가
-    logUserActivity(user.getId(), deviceId);
 
     String redirectUrlWithParams = UriComponentsBuilder.fromUriString(oAuth2Properties.getRedirectUrl())
             .queryParam("access", accessToken)
@@ -81,17 +76,9 @@ public class OAuth2AuthenticationSuccessHandler implements AuthenticationSuccess
     response.sendRedirect(redirectUrlWithParams);
   }
 
-  private void logUserActivity(Long userId, String deviceId) {
-    String logMessage = String.format("userId: %s, deviceId: %s, timestamp: %s", userId, deviceId, Instant.now());
-    try (FileWriter fw = new FileWriter(logPath, true)) {
-      fw.write(logMessage + "\n");
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-  }
-
   private String generateUniqueNickname(String nickname) {
     String uniqueSuffix = UUID.randomUUID().toString().split("-")[0];
+
     return userRepository.findByNickname(nickname)
             .map(existingUser -> nickname + "#" + uniqueSuffix)
             .orElse(nickname);
