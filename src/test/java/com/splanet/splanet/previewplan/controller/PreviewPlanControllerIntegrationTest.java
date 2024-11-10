7cd2f1a6e52622bb84aa7a75bc00d7883b5491d1
ez23re <ez23re@gmail.com>
2024-11-10 17:06:17 +0900

    test: 프리퓨를랜 통합테스트

--- src/test/java/com/splanet/splanet/previewplan/controller/PreviewPlanControllerIntegrationTest.java
+++ src/test/java/com/splanet/splanet/previewplan/controller/PreviewPlanControllerIntegrationTest.java
@@ -2,18 +2,22 @@ package com.splanet.splanet.previewplan.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jayway.jsonpath.JsonPath;
+import java.lang.reflect.Method;
 import com.splanet.splanet.jwt.JwtTokenProvider;
 import com.splanet.splanet.previewplan.dto.PlanCardRequestDto;
 import com.splanet.splanet.previewplan.dto.PlanCardResponseDto;
+import com.splanet.splanet.previewplan.repository.PlanCardRepository;
+import com.splanet.splanet.previewplan.repository.PlanGroupRepository;
 import com.splanet.splanet.previewplan.service.PreviewPlanService;
 import com.splanet.splanet.user.entity.User;
 import com.splanet.splanet.user.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
@@ -22,11 +26,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.HashSet;
+import java.util.Set;
 
-import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -49,6 +54,9 @@ class PreviewPlanControllerIntegrationTest {
 
     @Autowired
     private UserRepository userRepository;
+
+    @MockBean
+    private RedisTemplate<String, Object> redisTemplate;
 
     private String accessToken;
     private Long userId;
@@ -135,7 +143,7 @@ class PreviewPlanControllerIntegrationTest {
         PlanCardResponseDto planCardResponseDto = new PlanCardResponseDto(
                 "device-123",
                 "group-456",
-                cardId,  // 생성된 cardId를 사용
+                cardId,
                 "테스트 플랜 카드",
                 "플랜 카드 설명",
                 startTimestamp,
@@ -150,5 +158,87 @@ class PreviewPlanControllerIntegrationTest {
                 .andExpect(jsonPath("$.description").value(planCardResponseDto.description()))
                 .andExpect(jsonPath("$.startTimestamp").value(startTimestamp))
                 .andExpect(jsonPath("$.endTimestamp").value(endTimestamp));
+    }
+
+    @Test
+    @WithMockUser
+    void 플랜카드_수정_성공() throws Exception {
+        LocalDateTime now = LocalDateTime.now();
+        PlanCardRequestDto planCardRequestDto = new PlanCardRequestDto(
+                "테스트 플랜 카드",
+                "플랜 카드 설명",
+                now.toString(),
+                now.plusHours(2).toString()
+        );
+
+        long startTimestamp = now.atZone(ZoneId.systemDefault()).toEpochSecond();
+        long endTimestamp = now.plusHours(2).atZone(ZoneId.systemDefault()).toEpochSecond();
+
+        MvcResult result = mockMvc.perform(post("/api/preview-plan/card")
+                        .param("deviceId", "device-123")
+                        .param("groupId", "group-456")
+                        .header("Authorization", accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(planCardRequestDto)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String cardId = JsonPath.read(result.getResponse().getContentAsString(), "$.cardId");
+
+        PlanCardRequestDto updatedPlanCardRequestDto = new PlanCardRequestDto(
+                "수정된 플랜 카드",
+                "수정된 플랜 카드 설명",
+                now.plusHours(1).toString(),
+                now.plusHours(3).toString()
+        );
+
+        long updatedStartTimestamp = now.plusHours(1).atZone(ZoneId.systemDefault()).toEpochSecond();
+        long updatedEndTimestamp = now.plusHours(3).atZone(ZoneId.systemDefault()).toEpochSecond();
+
+        mockMvc.perform(put("/api/preview-plan/card/{deviceId}/{groupId}/{cardId}", "device-123", "group-456", cardId)
+                        .header("Authorization", accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatedPlanCardRequestDto)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.deviceId").value("device-123"))
+                .andExpect(jsonPath("$.groupId").value("group-456"))
+                .andExpect(jsonPath("$.title").value(updatedPlanCardRequestDto.title()))
+                .andExpect(jsonPath("$.description").value(updatedPlanCardRequestDto.description()))
+                .andExpect(jsonPath("$.startTimestamp").value(updatedStartTimestamp))
+                .andExpect(jsonPath("$.endTimestamp").value(updatedEndTimestamp));
+    }
+
+    @Test
+    @WithMockUser
+    void 플랜카드_삭제_성공() throws Exception {
+        LocalDateTime now = LocalDateTime.now();
+        PlanCardRequestDto planCardRequestDto = new PlanCardRequestDto(
+                "테스트 플랜 카드",
+                "플랜 카드 설명",
+                now.toString(),
+                now.plusHours(2).toString()
+        );
+
+        long startTimestamp = now.atZone(ZoneId.systemDefault()).toEpochSecond();
+        long endTimestamp = now.plusHours(2).atZone(ZoneId.systemDefault()).toEpochSecond();
+
+        MvcResult result = mockMvc.perform(post("/api/preview-plan/card")
+                        .param("deviceId", "device-123")
+                        .param("groupId", "group-456")
+                        .header("Authorization", accessToken)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(planCardRequestDto)))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String cardId = JsonPath.read(result.getResponse().getContentAsString(), "$.cardId");
+
+        mockMvc.perform(delete("/api/preview-plan/card/{deviceId}/{groupId}/{cardId}", "device-123", "group-456", cardId)
+                        .header("Authorization", accessToken))
+                .andExpect(status().isOk());
+
+        mockMvc.perform(get("/api/preview-plan/card/{deviceId}/{groupId}/{cardId}", "device-123", "group-456", cardId))
+                .andExpect(status().isNotFound())
+                .andExpect(jsonPath("$.message").value("플랜이 존재하지 않습니다."));
     }
 }
