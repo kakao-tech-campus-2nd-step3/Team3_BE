2ba83b8bec7183f7e2843724a63398ca4247eb8c
KimSongMok <mok0618@naver.com>
2024-11-10 21:18:55 +0900

    refactor: log기록 항목 추가

--- src/main/java/com/splanet/splanet/oauth/CustomOAuth2UserService.java
+++ src/main/java/com/splanet/splanet/oauth/CustomOAuth2UserService.java
@@ -1,6 +1,8 @@
 package com.splanet.splanet.oauth;
 
 import com.splanet.splanet.log.service.LogService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
@@ -13,14 +15,20 @@ import java.util.Map;
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
 
   private final LogService logService;
+  private HttpServletRequest request; // HttpServletRequest 필드 추가
 
+  @Autowired
   public CustomOAuth2UserService(LogService logService) {
     this.logService = logService;
   }
 
+  @Autowired // HttpServletRequest를 별도로 @Autowired로 주입
+  public void setRequest(HttpServletRequest request) {
+    this.request = request;
+  }
+
   @Override
   public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
-
     OAuth2User oAuth2User = super.loadUser(userRequest);
     Map<String, Object> attributes = oAuth2User.getAttributes();
 
@@ -31,9 +39,19 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     Long userId = (Long) attributes.get("id");
     String deviceId = oAuth2User.getName();
 
-    // 로그인 시 로그 기록
-    logService.recordLoginLog(userId, deviceId);
+    // 요청 경로 및 헤더 로깅 (예시)
+    String requestPath = request.getRequestURI();
+    String headers = getHeadersAsString(request);
+
+    logService.recordLoginLog(userId, deviceId, requestPath, headers);
 
     return oAuth2User;
+  }
+
+  private String getHeadersAsString(HttpServletRequest request) {
+    StringBuilder headers = new StringBuilder();
+    request.getHeaderNames().asIterator().forEachRemaining(headerName ->
+            headers.append(headerName).append(": ").append(request.getHeader(headerName)).append(", "));
+    return headers.toString();
   }
 }
