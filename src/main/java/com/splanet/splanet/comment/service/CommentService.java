449645cf82dba571ed5ca588e643542b93858bfb
ez23re <ez23re@gmail.com>
2024-11-10 14:50:50 +0900

    refactor: 원래대로 본인에게 댓글 달 수 있도록 수정

--- src/main/java/com/splanet/splanet/comment/service/CommentService.java
+++ src/main/java/com/splanet/splanet/comment/service/CommentService.java
@@ -31,15 +31,11 @@ public class CommentService {
 
     // 댓글 작성
     @Transactional
-    public void createComment(Long writerId, Long userId, CommentRequest request) {
-        User user = userRepository.findById(userId)
+    public void createComment(Long userId, CommentRequest request) {
+        User user = userRepository.findById(request.userId())
                 .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
-        User writer = userRepository.findById(writerId)
+        User writer = userRepository.findById(userId)
                 .orElseThrow(() -> new BusinessException(ErrorCode.USER_NOT_FOUND));
-
-        if (user.getId().equals(writer.getId())) {
-            throw new BusinessException(ErrorCode.INVALID_COMMENT_ID);
-        }
 
         Comment comment = Comment.builder()
                 .user(user)
