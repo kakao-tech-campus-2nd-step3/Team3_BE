ae4d7434982fae5df312bcb8b0a25205f23e0f9f
kanguk <lm0828@naver.com>
2024-11-13 10:40:07 +0900

    bug: 로깅 삭제 및 알림 형식 수정
    테스트 알림도 삭제

--- src/main/java/com/splanet/splanet/notification/scheduler/NotificationScheduler.java
+++ src/main/java/com/splanet/splanet/notification/scheduler/NotificationScheduler.java
@@ -26,29 +26,24 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class NotificationScheduler {
 
+    private static final ZoneId ZONE_ID_SEOUL = ZoneId.of("Asia/Seoul");
     private final PlanRepository planRepository;
     private final FcmTokenRepository fcmTokenRepository;
     private final NotificationLogRepository notificationLogRepository;
     private final NotificationService notificationService;
     private final QueryPerformanceService queryPerformanceService;
 
-    private static final ZoneId ZONE_ID_SEOUL = ZoneId.of("Asia/Seoul");
-
     @Scheduled(fixedRate = 60000)
     public void sendScheduledNotifications() {
         LocalDateTime now = LocalDateTime.now(ZONE_ID_SEOUL);
-        log.info("Scheduler started at {}", now);
 
         List<Plan> upcomingPlans = planRepository.findUpcomingPlans(now);
-        log.info("Found {} upcoming plans", upcomingPlans.size());
 
         Set<Long> userIds = upcomingPlans.stream()
                 .map(plan -> plan.getUser().getId())
                 .collect(Collectors.toSet());
-        log.info("Collected user IDs: {}", userIds);
 
         List<FcmToken> allFcmTokens = fcmTokenRepository.findByUserIdIn(userIds);
-        log.info("Fetched {} FCM tokens", allFcmTokens.size());
 
         Map<Long, List<FcmToken>> userFcmTokenMap = allFcmTokens.stream()
                 .collect(Collectors.groupingBy(fcmToken -> fcmToken.getUser().getId()));
@@ -56,15 +51,12 @@ public class NotificationScheduler {
         Set<Long> planIds = upcomingPlans.stream()
                 .map(Plan::getId)
                 .collect(Collectors.toSet());
-        log.info("Collected plan IDs: {}", planIds);
 
         Set<Long> fcmTokenIds = allFcmTokens.stream()
                 .map(FcmToken::getId)
                 .collect(Collectors.toSet());
-        log.info("Collected FCM token IDs: {}", fcmTokenIds);
 
         List<NotificationLog> notificationLogs = notificationLogRepository.findByFcmTokenIdInAndPlanIdIn(fcmTokenIds, planIds);
-        log.info("Found {} notification logs for existing notifications", notificationLogs.size());
 
         Set<String> sentNotificationKeys = notificationLogs.stream()
                 .map(log -> log.getFcmToken().getId() + ":" + log.getPlan().getId())
@@ -73,27 +65,18 @@ public class NotificationScheduler {
         for (Plan plan : upcomingPlans) {
             Long userId = plan.getUser().getId();
             List<FcmToken> fcmTokens = userFcmTokenMap.getOrDefault(userId, Collections.emptyList());
-            log.info("Processing plan ID {} for user ID {}, FCM tokens: {}", plan.getId(), userId, fcmTokens.size());
 
             for (FcmToken fcmToken : fcmTokens) {
                 if (Boolean.TRUE.equals(fcmToken.getIsNotificationEnabled())) {
                     LocalDateTime notificationTime = plan.getStartDate().atZone(ZONE_ID_SEOUL).toLocalDateTime().minusMinutes(fcmToken.getNotificationOffset());
-                    log.info("Evaluating notification for FCM token ID {} at notificationTime: {}", fcmToken.getId(), notificationTime);
 
                     if (notificationTime.isAfter(now.minusMinutes(5)) && notificationTime.isBefore(now.plusMinutes(1))) {
                         String notificationKey = fcmToken.getId() + ":" + plan.getId();
 
                         if (!sentNotificationKeys.contains(notificationKey)) {
-                            log.info("Sending notification for FCM token ID {} and plan ID {}", fcmToken.getId(), plan.getId());
                             notificationService.sendNotification(fcmToken, plan);
-                        } else {
-                            log.info("Notification already sent for FCM token ID {} and plan ID {}", fcmToken.getId(), plan.getId());
                         }
-                    } else {
-                        log.info("Notification time {} does not match the required range for now: {}", notificationTime, now);
                     }
-                } else {
-                    log.info("Notification disabled for FCM token ID {}", fcmToken.getId());
                 }
             }
         }
